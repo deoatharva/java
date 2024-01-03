8b5caea8ece7defc5723e507c9ba89bb80fbcf61
deoatharva <152968808+deoatharva@users.noreply.github.com>
2024-01-03 06:58:24 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,19 @@
+
+class datatypes {
+    public static void main(String[] args) {
+        byte b1=12;
+        short sh1=34;
+        int a=76;
+        char ch1='d';
+        float f1=34.45f;
+        boolean b2=true;
+        double db;
+        System.out.println("byte b1 "+b1);
+        System.out.println("short sh1 "+sh1);
+        System.out.println("int a "+a);
+        System.out.println("char ch1 "+ch1);
+        System.out.println("float f1 "+f1);
+        System.out.println("boolean b2 "+b2);
+       // System.out.println("double db"+db); error: variable db might not have been initialized
+    }
+}
