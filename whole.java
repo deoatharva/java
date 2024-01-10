a3671ce8b814fc90f29fa9e71e2aa676e02b34c6
deoatharva <152968808+deoatharva@users.noreply.github.com>
2024-01-10 21:48:20 +0530

    Add files via upload

---
+++ whole.java
@@ -0,0 +1,18 @@
+//to declare a variable of byte and check wether whole number or not
+import java.util.Scanner;
+class Whole {
+    public static void main(String args[]) {
+    Scanner scan = new Scanner(System.in);
+	System.out.println("enter b1");
+	int b1 = scan.nextInt();
+	if(b1>=0){
+	System.out.println("whole number");
+	}
+	else{
+	System.out.println("not whole number");
+    
+    }
+    
+        
+    }
+}
